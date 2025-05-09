32213ff0d67635dd535e5287750b581f8e60b4b9
Boom <kantapongok@gmail.com>
2025-05-09 12:19:11 +0700

    First Commit

---
+++ src/main/java/com/example/aws_ecr_app_deploy/AwsEcrAppDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_ecr_app_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcrAppDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEcrAppDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws_ecr_app_deploy/AwsEcrAppDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_ecr_app_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcrAppDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
